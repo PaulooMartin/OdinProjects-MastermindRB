283fe6945ded85936ee981ea6d4cbdaebd38ab99
Paul Martin <paulmartinblopez@gmail.com>
2023-05-06 13:53:01 +0800

    Add Game and Player classes with initialization

--- mastermind.rb
+++ mastermind.rb
@@ -1,1 +1,17 @@
-puts 'Hello!'
+class Game
+  KEYS = [1, 2, 3, 4, 5, 6, 7, 8].freeze
+
+  def initialize(guesser, maker)
+    @guesser = guesser
+    @maker = maker
+  end
+end
+
+class Player
+  attr_accessor :role
+
+  def initialize(initial_role, computer: false)
+    @role = initial_role
+    @is_computer = computer
+  end
+end
